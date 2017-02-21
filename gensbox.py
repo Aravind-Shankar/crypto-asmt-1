140d6c5b59e665b6b2b7fc01d19c58b72a78e478
--get <shankaravind167@gmail.com>
2017-02-21 17:14:20 +0530

    LAT and sbox-gen done

--- gensbox.py
+++ gensbox.py
@@ -2,7 +2,12 @@ import csv
 import sys
 import numpy as np
 
-# [1,2] - size, [3] - fname
-m,n = sys.argv[1:3]
+# [1,2] - size, [2] - fname
+m = int(sys.argv[1])
+n = int(sys.argv[2])
 fname = sys.argv[3]
 
+sbox = np.random.randint( 0, 2**n, ((2**(m/2)), (2**(m/2))) )
+with open(fname, "w") as f:
+    wr = csv.writer(f)
+    wr.writerows(sbox)
--- makeLAT.py
+++ makeLAT.py
@@ -12,7 +12,7 @@ def main():
         sys.exit()
     fname = sys.argv[1]
 
-    LEN_X, LEN_Y = 16, 10
+    LEN_X, LEN_Y = 8, 8
     X_RANGE, Y_RANGE = np.arange(2**LEN_X), np.arange(2**LEN_Y)
 
     with open(fname, "r") as f:
@@ -21,11 +21,16 @@ def main():
         for row in reader:
             sbox.append(row)
 
-    sbox = np.array(sbox).flatten()
-    sbox = np.vstack((X_RANGE, sbox)).transpose()
-
+    sbox = np.array(sbox, dtype=int).flatten()
+    
     A,B = np.meshgrid(X_RANGE, Y_RANGE)
     A,B = A.transpose(), B.transpose()
+
+    LAT = np.zeros( (len(A), len(B)) )
+    for x in X_RANGE:
+        y = sbox[x]
+        out = np.logical_not(np.bitwise_xor(lin_combo(A, x), lin_combo(B, y)))
+        LAT += out
 
     
 
